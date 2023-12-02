0d612555839e86d90a42ea823b5e5afdaab24487
Suman Bahadur Shrestha <44489993+SumanBahadurShrestha@users.noreply.github.com>
2023-12-02 11:29:14 +0545

    Initial commit

---
+++ app/mylibrary/src/main/java/com/omshanti/android/mylibrary/attention/BounceAnim.java
@@ -0,0 +1,12 @@
+package com.omshanti.android.mylibrary.attention;
+
+import android.view.View;
+
+import com.omshanti.android.mylibrary.BaseViewAnimator;
+
+public class BounceAnim extends BaseViewAnimator {
+    @Override
+    protected void prepare(View target) {
+
+    }
+}
